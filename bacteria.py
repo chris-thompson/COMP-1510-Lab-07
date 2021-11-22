61aeb0352e326a58b691334000c820dd9b6e549a
Chris <chris_thompson@bcit.ca>
2021-11-22 15:25:05 -0800

    Hello world!

---
+++ bacteria.py
@@ -0,0 +1,15 @@
+"""
+Bacteria.py contains your solution to lab 07.
+
+Good luck and have fun!
+"""
+
+"""
+Your comment goes here
+"""
+def colonize(surface_width: int, surface_height: int, x_start: int, y_start: int):
+    pass
+
+
+
+
